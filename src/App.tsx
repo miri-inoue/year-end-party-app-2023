2cf80d755439dcc68fbbc941440d35b7b6397182
takumi <hello.camiha@gmail.com>
2023-11-20 00:18:07 +0900

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,72 +4,68 @@ import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 
 function App() {
-	const [textMemberList, setTextMemberList] = useState<string>("");
-	const [textActiveMemberList, setTextActiveMemberList] = useState<string>("");
+	const [memberList, setMemberList] = useState<string[]>([]);
+	const [activeMemberList, setActiveMemberList] = useState<string[]>([]);
 
-	const [textPrizeList, setTextPrizeList] = useState<string>("");
-	const [textActivePrizeList, setTextActivePrizeList] = useState<string>("");
+	const [prizeList, setPrizeList] = useState<string[]>([]);
+	const [activePrizeList, setActivePrizeList] = useState<string[]>([]);
 
 	const [result, setResult] = useState<string>("");
-	const [textPreviousResultList, setTextPreviousResultList] =
-		useState<string>("");
+	const [previousResultList, setPreviousResultList] = useState<string[]>([]);
 
+	const [isRun, setIsRun] = useState<boolean>(false);
 	const [isFinish, setIsFinish] = useState<boolean>(false);
 
 	const isRunActive =
-		textMemberList !== "" && textPrizeList !== "" && !isFinish;
+		memberList.length !== 0 && prizeList.length !== 0 && !isFinish;
 
 	const handleRun = () => {
-		// resolve member
-		if (textActiveMemberList === "") setTextActiveMemberList(textMemberList);
-		const activeMemberList =
-			textActiveMemberList === ""
-				? textMemberList.split("\n")
-				: textActiveMemberList.split("\n");
+		// initial click (load data)
+		if (activeMemberList.length === 0 || activePrizeList.length === 0) {
+			setActiveMemberList(memberList);
+			setActivePrizeList(prizeList);
+			setIsRun(true);
+			return;
+		}
 
+		// resolve member
 		const randomMemberIndex = Math.floor(
 			Math.random() * activeMemberList.length,
 		);
-		const result = activeMemberList[randomMemberIndex];
+		const member = activeMemberList[randomMemberIndex];
 		const nextActiveMemberList = activeMemberList.filter(
-			(item) => item !== result,
+			(item) => item !== member,
 		);
-		const textNextActiveMemberList = nextActiveMemberList.join("\n");
 
 		// resolve prize
-		if (textActivePrizeList === "") setTextActivePrizeList(textPrizeList);
-		const activePrizeList =
-			textActivePrizeList === ""
-				? textPrizeList.split("\n")
-				: textActivePrizeList.split("\n");
-
 		const randomPrizeIndex = Math.floor(Math.random() * activePrizeList.length);
 		const prize = activePrizeList[randomPrizeIndex];
 		const nextActivePrizeList = activePrizeList.filter(
 			(item) => item !== prize,
 		);
-		const textNextActivePrizeList = nextActivePrizeList.join("\n");
+
+		setResult(`${member} - ${prize}`);
+		setPreviousResultList((prev) =>
+			prev.length === 0
+				? [`${member} - ${prize}`]
+				: [...prev, `${member} - ${prize}`],
+		);
+		setActiveMemberList(nextActiveMemberList);
+		setActivePrizeList(nextActivePrizeList);
 
 		const isMemberEmpty = nextActiveMemberList.length === 0;
 		const isPrizeEmpty = nextActivePrizeList.length === 0;
-
-		setResult(`${result} - ${prize}`);
-
-		const resolvedTextPreviousResultList =
-			textPreviousResultList === ""
-				? `${result} - ${prize}`
-				: `${textPreviousResultList}\n${result} - ${prize}`;
-		setTextPreviousResultList(resolvedTextPreviousResultList);
 		setIsFinish(isMemberEmpty || isPrizeEmpty);
-		setTextActiveMemberList(textNextActiveMemberList);
-		setTextActivePrizeList(textNextActivePrizeList);
 	};
 
 	const handleReset = () => {
-		setTextActiveMemberList("");
-		setTextActivePrizeList("");
+		setActiveMemberList([]);
+		setActivePrizeList([]);
+
 		setResult("");
-		setTextPreviousResultList("");
+		setPreviousResultList([]);
+
+		setIsRun(false);
 		setIsFinish(false);
 	};
 
@@ -80,19 +76,21 @@ function App() {
 				<div className="grid w-full gap-2">
 					<Label htmlFor="member-list">Member list</Label>
 					<Textarea
-						onChange={(e) => setTextMemberList(e.target.value)}
+						onChange={(e) => setMemberList(e.target.value.trim().split("\n"))}
 						className="h-40"
 						placeholder="Type member list here."
 						id="member-list"
+						disabled={isRun}
 					/>
 				</div>
 				<div className="grid w-full gap-2">
 					<Label htmlFor="prize-list">Prize list</Label>
 					<Textarea
-						onChange={(e) => setTextPrizeList(e.target.value)}
+						onChange={(e) => setPrizeList(e.target.value.trim().split("\n"))}
 						className="h-40"
 						placeholder="Type prize list here."
 						id="prize-list"
+						disabled={isRun}
 					/>
 				</div>
 			</div>
@@ -100,7 +98,7 @@ function App() {
 				<div className="grid w-full gap-2">
 					<Label htmlFor="active-member-list">Active member list</Label>
 					<Textarea
-						value={textActiveMemberList}
+						value={activeMemberList.join("\n")}
 						className="h-40"
 						placeholder="active member list here."
 						id="active-member"
@@ -110,7 +108,7 @@ function App() {
 				<div className="grid w-full gap-2">
 					<Label htmlFor="active-prize-list">Active prize list</Label>
 					<Textarea
-						value={textActivePrizeList}
+						value={activePrizeList.join("\n")}
 						className="h-40"
 						placeholder="active prize list here."
 						id="active-prize"
@@ -122,7 +120,7 @@ function App() {
 				<div className="grid w-full gap-2">
 					<Label htmlFor="previous-result">Previous result</Label>
 					<Textarea
-						value={textPreviousResultList}
+						value={previousResultList.join("\n")}
 						className="h-40"
 						placeholder="Previous Result here."
 						id="previous-result"
@@ -142,7 +140,7 @@ function App() {
 			</div>
 			<div className="grid w-full gap-2">
 				{isRunActive ? (
-					<Button onClick={handleRun}>Run</Button>
+					<Button onClick={handleRun}>{isRun ? "Run" : "Load"}</Button>
 				) : (
 					<Button disabled>Run</Button>
 				)}
