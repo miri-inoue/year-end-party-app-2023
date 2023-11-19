6c59d0040c1091d438244011fd3f41d0d80cbd82
takumi <hello.camiha@gmail.com>
2023-11-19 18:57:28 +0900

    set gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
+	base: "/year-end-party-app-2023/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
