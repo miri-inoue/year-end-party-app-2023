1e8aed0fc068f56f29e86fd9a58c5b596a49a637
takumi <hello.camiha@gmail.com>
2023-11-20 00:40:49 +0900

    update styling

--- src/App.tsx
+++ src/App.tsx
@@ -70,91 +70,104 @@ function App() {
 	};
 
 	return (
-		<div className="flex flex-col gap-4 p-8 h-screen justify-between">
-			<div className="flex gap-2 items-baseline">
-				<h1 className="text-xl">Year end party 2023, Lottery app</h1>
-				<a
-					className="text-xs text-blue-500 hover:underline"
-					href="https://github.com/camiha/year-end-party-app-2023"
-				>
-					(source code here)
-				</a>
+		<div className="flex flex-col gap-12 p-8 h-screen justify-between">
+			<div className="flex flex-col gap-8">
+				<div className="flex gap-2 items-baseline">
+					<h1 className="text-xl">Year end party 2023, Lottery app</h1>
+					<a
+						className="text-xs text-blue-500 hover:underline"
+						href="https://github.com/camiha/year-end-party-app-2023"
+					>
+						(source code here)
+					</a>
+				</div>
+				<div className="flex flex-col gap-4">
+					<div className="flex gap-2">
+						<div className="grid w-full gap-2">
+							<Label htmlFor="member-list">Member list</Label>
+							<Textarea
+								onChange={(e) =>
+									setMemberList(e.target.value.trim().split("\n"))
+								}
+								className="h-40"
+								placeholder="Type member list here."
+								id="member-list"
+								disabled={isRun}
+							/>
+						</div>
+						<div className="grid w-full gap-2">
+							<Label htmlFor="prize-list">Prize list</Label>
+							<Textarea
+								onChange={(e) =>
+									setPrizeList(e.target.value.trim().split("\n"))
+								}
+								className="h-40"
+								placeholder="Type prize list here."
+								id="prize-list"
+								disabled={isRun}
+							/>
+						</div>
+					</div>
+					<div className="flex gap-2">
+						<div className="grid w-full gap-2">
+							<Label htmlFor="active-member-list">Active member list</Label>
+							<Textarea
+								value={activeMemberList.join("\n")}
+								className="h-40"
+								placeholder="Active member list here."
+								id="active-member"
+								disabled
+							/>
+						</div>
+						<div className="grid w-full gap-2">
+							<Label htmlFor="active-prize-list">Active prize list</Label>
+							<Textarea
+								value={activePrizeList.join("\n")}
+								className="h-40"
+								placeholder="Active prize list here."
+								id="active-prize"
+								disabled
+							/>
+						</div>
+					</div>
+					<div className="flex gap-2">
+						<div className="grid w-full gap-2">
+							<Label htmlFor="previous-result">Previous result</Label>
+							<Textarea
+								value={previousResultList.join("\n")}
+								className="h-40"
+								placeholder="Previous Result here."
+								id="previous-result"
+								disabled
+							/>
+						</div>
+						<div className="grid w-full gap-2">
+							<Label htmlFor="result">Current result</Label>
+							<Textarea
+								value={result}
+								className="h-40"
+								placeholder="Result here."
+								id="result"
+								disabled
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="flex flex-col gap-4">
-				<div className="flex gap-2">
-					<div className="grid w-full gap-2">
-						<Label htmlFor="member-list">Member list</Label>
-						<Textarea
-							onChange={(e) => setMemberList(e.target.value.trim().split("\n"))}
-							className="h-40"
-							placeholder="Type member list here."
-							id="member-list"
-							disabled={isRun}
-						/>
-					</div>
-					<div className="grid w-full gap-2">
-						<Label htmlFor="prize-list">Prize list</Label>
-						<Textarea
-							onChange={(e) => setPrizeList(e.target.value.trim().split("\n"))}
-							className="h-40"
-							placeholder="Type prize list here."
-							id="prize-list"
-							disabled={isRun}
-						/>
-					</div>
-				</div>
-				<div className="flex gap-2">
-					<div className="grid w-full gap-2">
-						<Label htmlFor="active-member-list">Active member list</Label>
-						<Textarea
-							value={activeMemberList.join("\n")}
-							className="h-40"
-							placeholder="Active member list here."
-							id="active-member"
-							disabled
-						/>
-					</div>
-					<div className="grid w-full gap-2">
-						<Label htmlFor="active-prize-list">Active prize list</Label>
-						<Textarea
-							value={activePrizeList.join("\n")}
-							className="h-40"
-							placeholder="Active prize list here."
-							id="active-prize"
-							disabled
-						/>
-					</div>
-				</div>
-				<div className="flex gap-2">
-					<div className="grid w-full gap-2">
-						<Label htmlFor="previous-result">Previous result</Label>
-						<Textarea
-							value={previousResultList.join("\n")}
-							className="h-40"
-							placeholder="Previous Result here."
-							id="previous-result"
-							disabled
-						/>
-					</div>
-					<div className="grid w-full gap-2">
-						<Label htmlFor="result">Current result</Label>
-						<Textarea
-							value={result}
-							className="h-40"
-							placeholder="Result here."
-							id="result"
-							disabled
-						/>
-					</div>
-				</div>
-			</div>
-			<div className="grid w-full gap-2">
+
+			<div className="flex w-full gap-2">
 				{isRunActive ? (
-					<Button onClick={handleRun}>{isRun ? "Run" : "Load"}</Button>
+					<Button className="w-full" onClick={handleRun}>
+						{isRun ? "Run" : "Load"}
+					</Button>
 				) : (
-					<Button disabled>Run</Button>
+					<Button className="w-full" disabled>
+						Run
+					</Button>
 				)}
-				<Button onClick={handleReset}>Reset</Button>
+				<Button className="w-full" onClick={handleReset}>
+					Reset
+				</Button>
 			</div>
 		</div>
 	);
