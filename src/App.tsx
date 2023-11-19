6f2d98f42bd34444ba5db28b0981db5ddd1f918c
takumi <hello.camiha@gmail.com>
2023-11-19 18:36:47 +0900

    create prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,67 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 function App() {
+	const [textMemberList, setTextMemberList] = useState<string>("");
+	const [textTargetList, setTextTargetList] = useState<string>("");
+	const [winner, setWinner] = useState<string>("");
+	const [isFinish, setIsFinish] = useState<boolean>(false);
+
+	const isRunActive = textMemberList !== "" && !isFinish;
+
+	const handleRun = () => {
+		if (textTargetList === "") setTextTargetList(textMemberList);
+
+		const targetList =
+			textTargetList === ""
+				? textMemberList.split("\n")
+				: textTargetList.split("\n");
+
+		const randomIndex = Math.floor(Math.random() * targetList.length);
+		const winner = targetList[randomIndex];
+
+		const nextTargetList = targetList.filter((item) => item !== winner);
+		const textNextTargetList = nextTargetList.join("\n");
+
+		setIsFinish(nextTargetList.length === 0);
+		setWinner(winner);
+		setTextTargetList(textNextTargetList);
+	};
+
+	const handleReset = () => {
+		setWinner("");
+		setIsFinish(false);
+	};
+
 	return (
 		<div className="flex flex-col gap-4 p-8">
-			<h1 className="text-xl">year end party 2023, lottery app (prototype)</h1>
+			<h1 className="text-xl">Year end party 2023, Lottery app</h1>
 
 			<div className="grid w-full gap-2">
 				<Label htmlFor="member-list">Member list</Label>
 				<Textarea
-					className="h-40"
+					onChange={(e) => setTextMemberList(e.target.value)}
+					className="h-60"
 					placeholder="Type member list here."
 					id="member-list"
 				/>
 			</div>
 			<div className="grid w-full gap-2">
+				<Label htmlFor="target-list">Target list</Label>
+				<Textarea
+					value={textTargetList}
+					className="h-60"
+					placeholder="Current target list here."
+					id="member-list"
+					disabled
+				/>
+			</div>
+			<div className="grid w-full gap-2">
 				<Label htmlFor="winner">Winner</Label>
 				<Textarea
+					value={winner}
 					className="h-20"
 					placeholder="Result here."
 					id="winner"
@@ -25,17 +69,12 @@ function App() {
 				/>
 			</div>
 			<div className="grid w-full gap-2">
-				<Label htmlFor="previous-winner">Previous winner</Label>
-				<Textarea
-					className="h-40"
-					placeholder="Result here."
-					id="Winner"
-					disabled
-				/>
-			</div>
-			<div className="grid w-full gap-2">
-				<Button>Run</Button>
-				<Button>Reset</Button>
+				{isRunActive ? (
+					<Button onClick={handleRun}>Run</Button>
+				) : (
+					<Button disabled>Run</Button>
+				)}
+				<Button onClick={handleReset}>Reset</Button>
 			</div>
 		</div>
 	);
