978c2284266fc600606d9ad1d989ea73c4ee959a
Miri Inoue <miri.inoue@un-t.com>
2023-12-17 15:59:33 +0900

    fix: ルーレットの矢印の位置を3時の位置に変更

--- src/hooks/useLottery.ts
+++ src/hooks/useLottery.ts
@@ -4,6 +4,52 @@ import { getResultListOutput } from "../reducer/functions";
 import { initialState, rouletteReducer } from "../reducer/roulette";
 
 export const useLottery = () => {
+
+	const option = {
+		size: 100,
+		maxSpeed: 100,
+		rotationDirection: "clockwise",
+		acceleration: 1,
+		deceleration: 1,
+		initialAngle: 0,
+		determineAngle: 45,
+		showArrow: true,
+		style: {
+			canvas: {
+				bg: "#fff",
+			},
+			arrow: {
+				bg: "#000",
+				size: 16,
+			},
+			label: {
+				font: "16px Arial",
+				align: "right",
+				baseline: "middle",
+				offset: 0.75,
+				defaultColor: "#000",
+			},
+			pie: {
+				theme: [
+					{
+						bg: "#DA4193",
+					},
+					{
+						bg: "#E24A35",
+					},
+					{
+						bg: "#6366f1",
+						color: "#fff",
+					},
+					{
+						bg: "#4338ca",
+						color: "#fff",
+					},
+				],
+			},
+		},
+	};
+
 	const [state, dispatch] = useReducer(rouletteReducer, initialState);
 	const {
 		items,
@@ -25,7 +71,58 @@ export const useLottery = () => {
 		items,
 		options: {
 			size: 600,
+			determineAngle: 0,
+			style: {
+				canvas: {
+					bg: "transparent",
+				},
+				arrow: {
+					bg: "#000",
+					size: 16,
+				},
+				label: {
+					font: "16px Arial",
+					align: "right",
+					baseline: "middle",
+					offset: 0.75,
+					defaultColor: "#000",
+				},
+				pie: {
+					theme: [
+						{
+							bg: "#DA4193",
+							color: "#fff",
+						},
+						{
+							bg: "#E24A35",
+							color: "#fff",
+						},
+						{
+							bg: "#E4D528",
+						},
+						{
+							bg: "#85BD40",
+						},
+						{
+							bg: "#19AB9C",
+						},
+						{
+							bg: "#177EC2",
+							color: "#fff",
+						},
+						{
+							bg: "#4A469F",
+							color: "#fff",
+						},
+						{
+							bg: "#9F4C99",
+							color: "#fff",
+						},
+					],
+				},
+			},
 		},
+
 		onSpinEnd: (result) => {
 			dispatch({ type: "ON_SPIN_COMPLETE", result });
 		},
